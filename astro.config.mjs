8cd8bcc9fe452d77bfad3bcfe78a19dc4eff4cf2
miyabi-0418 <yuzurina.miyabi@gmail.com>
2026-05-24 19:44:06 +0900

    change repository name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: "https://miyabi-0418.github.io",
-  base: "/second-shepherd",
 
   vite: {
     plugins: [tailwindcss()]
